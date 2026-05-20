a37a2dbd4534b9ef9f97897ad18dbea176b011a9
brunodreyfus-rgb <bruno.dreyfus@gmail.com>
2026-05-20 15:50:45 +0300

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,2 +1,4 @@
 const SUPABASE_URL = "https://rkknttzzrrjavbyymvws.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_nso5Ruf3kvtEFH8FFlpzEQ_CY9DCUsm";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_nso5Ruf3kvtEFH8FFlpzEQ_CY9DCUsm";
